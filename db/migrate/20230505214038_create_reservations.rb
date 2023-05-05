5e5714b13611984d3ef6724704758448e915c860
ma-sss <mskk0522@icloud.com>
2023-05-06 07:09:38 +0900

    reservationモデル作成

---
+++ db/migrate/20230505214038_create_reservations.rb
@@ -0,0 +1,13 @@
+class CreateReservations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :reservations do |t|
+      t.date "day", null: false
+      t.bigint "admin_id", null: false
+      t.bigint "user_id", null: false
+      t.datetime "created_at", precision: 6, null: false
+      t.datetime "updated_at", precision: 6, null: false
+      t.datetime "start_time", null: false
+      t.timestamps
+    end
+  end
+end
