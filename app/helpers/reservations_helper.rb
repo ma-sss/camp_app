9938e2268c4c6811d1e42149f692d4c861ae344e
ma-sss <mskk0522@icloud.com>
2023-05-07 06:58:44 +0900

    カレンダー表示のため、month_calendar,reservations_helper.rbを修正

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -1,2 +1,19 @@
 module ReservationsHelper
+
+    def check_reservation(reservations, day)
+        result = false
+        reservations_count = reservations.count
+        # 取得した予約データにdayとtimeが一致する場合はtrue,一致しない場合はfalseを返します
+        if reservations_count > 1
+          reservations.each do |reservation|
+            result = reservation[:day].eql?(day.strftime("%Y-%m-%d"))
+            return result if result
+          end
+        elsif reservations_count == 1
+          result = reservations[0][:day].eql?(day.strftime("%Y-%m-%d"))
+          return result if result
+        end
+        return result
+      end
+
 end
