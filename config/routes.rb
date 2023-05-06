c70c515c7149502d0aefff1ba1f2f7df8123677b
ma-sss <mskk0522@icloud.com>
2023-05-06 11:51:30 +0900

    resources :admins,resources :reservationsを追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
   get '/' => 'homes#top'
+  
   devise_for :admins, controllers: {
     sessions:      'admins/sessions',
     passwords:     'admins/passwords',
     registrations: 'admins/registrations'
   }
+
   devise_for :users, controllers: {
     sessions:      'users/sessions',
     passwords:     'users/passwords',
     registrations: 'users/registrations'
   }
-end
+
+  resources :admins, :only => [:index, :show] do
+    resources :reservations
+  end
+  end
