0632652c7b1ddc32585a33d73be22c33d2357ffa
ma-sss <mskk0522@icloud.com>
2023-05-07 06:26:27 +0900

    routes修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,28 @@
 Rails.application.routes.draw do
-  get '/' => 'homes#top'
+  get "/" => "homes#top"
+  get "reservations/user_reservations" => "reservations#user_reservations"
   
   devise_for :admins, controllers: {
     sessions:      'admins/sessions',
     passwords:     'admins/passwords',
     registrations: 'admins/registrations'
   }
-
+  
   devise_for :users, controllers: {
     sessions:      'users/sessions',
     passwords:     'users/passwords',
     registrations: 'users/registrations'
   }
-
+  
   resources :admins, :only => [:index, :show] do
     resources :reservations
   end
-
+  
+  resources :admins, expect: [:index]
+  
   resources :favorites, only: [:index, :create, :destroy]
+
+  resources :users, only: [:show, :edit, :update] do
+  end
+
   end
