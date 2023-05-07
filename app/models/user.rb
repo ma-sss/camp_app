415c8dc75e8ce9165c65a4d6f5fd88788b8731ae
ma-sss <mskk0522@icloud.com>
2023-05-07 09:54:24 +0900

    models/admin,user を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)(?=.*?[!-\/:-@\[-`{-~\\]])[!-~]+\z/i
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/
   validates :password, format: { with: VALID_PASSWORD_REGEX ,
-      message: "は半角英小文字・大文字・数字・記号を含めて設定してください" }
+      message: "は半角6~12文字英大文字・小文字・数字それぞれ1文字以上含む必要があります" }
   validates :email, presence: true, length: { maximum: 255 },uniqueness: true
 
   has_many :favorites, dependent: :destroy
