77dd951b752662cb796414bce553197b52e66eb1
ma-sss <mskk0522@icloud.com>
2023-05-13 15:08:30 +0900

    app/models/admin.rb修正

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -8,6 +8,10 @@ class Admin < ApplicationRecord
   validates :password, format: { with: VALID_PASSWORD_REGEX ,
       message: "は半角6~12文字英大文字・小文字・数字それぞれ1文字以上含む必要があります" }
   validates :email, presence: true, length: { maximum: 255 },uniqueness: true
+  validates :name, presence: true, length: { maximum: 50 },uniqueness: true
+  validates :address, presence: true, length: { maximum: 50 },uniqueness: true
+  VALID_TELEPHONE_NUMBER_REGEX = /\A0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1})[-)]?\d{4}\z|\A0[5789]0[-]?\d{4}[-]?\d{4}\z/
+  validates :telephone_number, presence: true, format: { with: VALID_TELEPHONE_NUMBER_REGEX, message: "を正しい形式で入力してください。" }, uniqueness: true
 
   has_many :reservations
   has_many :favorites, dependent: :destroy
