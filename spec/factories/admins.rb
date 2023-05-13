4c44ee9d24341da305d8f8d9758d37c5d2a850e2
ma-sss <mskk0522@icloud.com>
2023-05-13 15:06:37 +0900

    spec/factories/admins.rb作成と修正

---
+++ spec/factories/admins.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+    factory :admin do
+      email { "test@example.com" }
+      password              { "Password123" }
+      password_confirmation { "Password123" }
+      name { "キャンプ場123" }
+      address { "東京" }
+      telephone_number { "090-0000-0000" }
+    end
+  end
