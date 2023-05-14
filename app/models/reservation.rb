ab9067fbd5952e725f3f6fe9beb4808f20ef2823
ma-sss <mskk0522@icloud.com>
2023-05-14 14:13:36 +0900

    app/models/reservation.rb修正

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -15,19 +15,36 @@ class Reservation < ApplicationRecord
       reservation_data
   end
 
+  validates :day, presence: true
+  validates :admin_id, presence: true
+  validates :user_id, presence: true
+  validates :start_time, presence: true
+  validates :admin, presence: true
   validate :date_before_start
   validate :date_current_today
   validate :date_three_month_end
 
   def date_before_start
-    errors.add(:day, "は過去の日付は選択できません") if day < Date.current
+    if day.nil?
+      errors.add(:day, "は必須項目です")
+    elsif day < Date.current
+      errors.add(:day, "は過去の日付は選択できません")
+    end
   end
 
   def date_current_today
-    errors.add(:day, "は当日は選択できません。予約画面から正しい日付を選択してください。") if day < (Date.current + 1)
+    if day.nil?
+      errors.add(:day, "は必須項目です")
+    elsif day < (Date.current + 1)
+      errors.add(:day, "は当日は選択できません")
+    end
   end
 
   def date_three_month_end
-    errors.add(:day, "は3ヶ月以降の日付は選択できません") if (Date.current >> 3) < day
+    if day.nil?
+      errors.add(:day, "は必須項目です")
+    elsif (Date.current >> 3) < day
+      errors.add(:day, "は3ヶ月以降の日付は選択できません")
+    end
   end
 end
