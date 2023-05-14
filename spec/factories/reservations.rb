8fbff088864f088e0f76a848978599a52db76465
ma-sss <mskk0522@icloud.com>
2023-05-14 14:35:50 +0900

    不要ファイル削除

--- spec/factories/reservations.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-    factory :reservation do
-      day {Date.current + 1}
-      admin_id {1}
-      user_id {1}
-      start_time {Date.current + 1}
-      association :admin, factory: :admin
-    end
-  end
