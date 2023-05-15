9a92d7bf66e0b5b709896c1b9abe0956f559ca6f
ma-sss <mskk0522@icloud.com>
2023-05-16 06:54:38 +0900

    spec/factories/users.rb修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
     factory :user do
+      name { "user1" }
       email { "test@example.com" }
       password              { "Password123" }
       password_confirmation { "Password123" }
