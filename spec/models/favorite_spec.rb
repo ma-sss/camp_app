38cf6bc537bf0df84403e42916e29f249fe8d71a
ma-sss <mskk0522@icloud.com>
2023-05-16 06:55:23 +0900

    spec/models/favorite_spec.rb修正

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -5,7 +5,7 @@ describe Favorite do
     it "admin_id,user_idがある場合は登録できること" do
       admin = Admin.create(email: "test@example.com", password: "Password123", name: "キャンプ場123",
             address: "東京", telephone_number: "090-0000-0000")
-      user = User.create(email: "test@example.com", password: "Password123")
+      user = User.create(name: "user1", email: "test@example.com", password: "Password123")
       favorite = Favorite.create(admin_id: admin.id, user_id: user.id)
       expect(favorite).to be_valid
     end
@@ -13,7 +13,7 @@ describe Favorite do
     it "admin_idがない場合は登録できないこと" do
       admin = Admin.create(email: "test@example.com", password: "Password123", name: "キャンプ場123",
             address: "東京", telephone_number: "090-0000-0000")
-      user = User.create(email: "test@example.com", password: "Password123")
+      user = User.create(name: "user1", email: "test@example.com", password: "Password123")
       favorite = Favorite.create(admin_id: nil, user_id: user.id)
       favorite.valid?
       expect(favorite.errors[:admin_id]).to include("を入力してください")
@@ -22,7 +22,7 @@ describe Favorite do
     it "user_idがない場合は登録できないこと" do
       admin = Admin.create(email: "test@example.com", password: "Password123", name: "キャンプ場123",
             address: "東京", telephone_number: "090-0000-0000")
-      user = User.create(email: "test@example.com", password: "Password123")
+      user = User.create(name: "user1", email: "test@example.com", password: "Password123")
       favorite = Favorite.create(admin_id: admin.id, user_id: nil)
       favorite.valid?
       expect(favorite.errors[:user_id]).to include("を入力してください")
