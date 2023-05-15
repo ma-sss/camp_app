f53bb6b314bb5a16f942f23ff596237f9d95d8e8
ma-sss <mskk0522@icloud.com>
2023-05-16 06:56:18 +0900

    spec/models/user_spec.rb修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,44 +3,64 @@ describe User do
   describe '#create' do
 
     it "emailとpasswordがある場合は登録できること" do
-        user = User.create(email: "test@example.com", password: "Password123")
+        user = User.create(name: "user1", email: "test@example.com", password: "Password123")
         user.valid?
         expect(user).to be_valid
     end
 
+    it "nameがない場合は登録できないこと" do
+        user = User.create(name: nil, email: "test@example.com", password: "Password123")
+        user.valid?
+        expect(user.errors[:name]).to include("を入力してください")
+    end
+
     it "emailがない場合は登録できないこと" do
-        user = User.create(email: nil, password: "Password123")
+        user = User.create(name: "user1", email: nil, password: "Password123")
         user.valid?
         expect(user.errors[:email]).to include("を入力してください")
     end
 
     it "passwordがない場合は登録できないこと" do
-        user = User.create(email: "test@example.com", password: nil)
+        user = User.create(name: "user1", email: "test@example.com", password: nil)
         user.valid?
         expect(user.errors[:password]).to include("を入力してください")
     end
 
     it "emailとpasswordがない場合は登録できないこと" do
-        user = User.create(email: nil, password: nil)
+        user = User.create(name: "user1", email: nil, password: nil)
         user.valid?
         expect(user.errors[:email]).to include("を入力してください")
     end
 
     it "emailに@がない場合は登録できないこと" do
-        user = User.create(email: "testexample.com", password: nil)
+        user = User.create(name: "user1", email: "testexample.com", password: nil)
         user.valid?
         expect(user.errors[:email]).to include("は不正な値です")
     end
 
-    it "emailが256文字以上の場合は登録できないこと" do
-        user = User.create(email: "testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest@example.com",
+    it "userが30文字以上の場合は登録できないこと" do
+        user = User.create(name: "1user1user1user1user1user1user1user1user1user1user1", email: "test@example.com",
              password: nil)
+        user.valid?
+        expect(user.errors[:name]).to include("は30文字以内で入力してください")
+    end
+
+    it "emailが256文字以上の場合は登録できないこと" do
+        user = User.create(name: "user1", email: "testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest@example.com",
+             password: "Password123")
         user.valid?
         expect(user.errors[:email]).to include("は255文字以内で入力してください")
     end
 
+    it "nameが重複している場合は登録できないこと" do
+        user = User.create(name: "user1", email: "test@example.com", password: "Password123")
+        user1 = FactoryBot.build(:user)
+        user1.valid?
+        expect(user1.errors[:name]).to include("はすでに存在します")
+    end
+
     it "emailが重複している場合は登録できないこと" do
-        user = User.create(email: "test@example.com", password: "Password123")
+        user = User.create(name: "user1", email: "test@example.com", password: "Password123")
         user1 = FactoryBot.build(:user)
         user1.valid?
         expect(user1.errors[:email]).to include("はすでに存在します")
