3e182064278bd64580267c53989b979722ddf74e
ma-sss <mskk0522@icloud.com>
2023-05-17 20:43:34 +0900

    app/controllers/admins_controller.rbにbefore_action :authenticate_admin!とadmin_reservations追記

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,7 +1,20 @@
 class AdminsController < ApplicationController
 
+    before_action :authenticate_admin!, only:[:admin_reservations]
+
     def show
         @admin = Admin.find_by(id: params[:id])
+    end
+
+    def admin_reservations
+        if params[:day].present?
+            if @reservation = Reservation.find_by(day: params[:day])
+                @user = User.find_by(id: @reservation.user_id)
+            end
+        else
+            @reservations = Reservation.all
+            @users = User.all
+        end
     end
     
 end
