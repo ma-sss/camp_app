3f8002cbcbd59a881d6b7bdbb6e20de0031a51d1
ma-sss <mskk0522@icloud.com>
2023-05-20 06:31:25 +0900

    spec/factories/admins.rb,spec/factories/users.rbインデント修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
-    factory :user do
-      name { "user1" }
-      email { "test@example.com" }
-      password              { "Password123" }
-      password_confirmation { "Password123" }
-    end
+  factory :user do
+    name { "user1" }
+    email { "test@example.com" }
+    password              { "Password123" }
+    password_confirmation { "Password123" }
   end
+end
