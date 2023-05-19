b7c19f325a8e5843bb0dec1971f679a73c5b4c0e
ma-sss <mskk0522@icloud.com>
2023-05-20 06:29:39 +0900

    dayとstart_timeの重複テストを追加したためspec/models/reservaiton_spec.rb修正

--- spec/models/reservaiton_spec.rb
+++ spec/models/reservaiton_spec.rb
@@ -46,13 +46,39 @@ describe Reservation do
       expect(reservation.errors[:start_time]).to include("を入力してください")
     end
 
+    it "dayが重複している場合は登録できないこと" do
+      admin = Admin.create(email: "test@example.com", password: "Password123", name: "キャンプ場123",
+            address: "東京", telephone_number: "090-0000-0000")
+      user = User.create(name: "user1", email: "test@example.com", password: "Password123")
+      admin2 = Admin.create(email: "test2@example.com", password: "Password123", name: "キャンプ場1234",
+        address: "東京2", telephone_number: "090-2000-0000")
+      user2 = User.create(name: "user2", email: "test2@example.com", password: "Password123")
+      reservation = Reservation.create(day: Date.current + 2, admin_id: admin.id, user_id: user.id, start_time: Date.current + 2)
+      reservation1 = Reservation.create(day: Date.current + 2, admin_id: admin2.id, user_id: user2.id, start_time: Date.current + 3)
+      reservation1.valid?
+      expect(reservation1.errors[:day]).to include("はすでに存在します")
+    end
+
+    it "start_timeが重複している場合は登録できないこと" do
+      admin = Admin.create(email: "test@example.com", password: "Password123", name: "キャンプ場123",
+            address: "東京", telephone_number: "090-0000-0000")
+      user = User.create(name: "user1", email: "test@example.com", password: "Password123")
+      admin2 = Admin.create(email: "test2@example.com", password: "Password123", name: "キャンプ場1234",
+        address: "東京2", telephone_number: "090-2000-0000")
+      user2 = User.create(name: "user2", email: "test2@example.com", password: "Password123")
+      reservation = Reservation.create(day: Date.current + 2, admin_id: admin.id, user_id: user.id, start_time: Date.current + 2)
+      reservation1 = Reservation.create(day: Date.current + 3, admin_id: admin2.id, user_id: user2.id, start_time: Date.current + 2)
+      reservation1.valid?
+      expect(reservation1.errors[:start_time]).to include("はすでに存在します")
+    end
+
     it "dayが過去の場合は登録できないこと" do
       admin = Admin.create(email: "test@example.com", password: "Password123", name: "キャンプ場123",
             address: "東京", telephone_number: "090-0000-0000")
       user = User.create(name: "user1", email: "test@example.com", password: "Password123")
       reservation = Reservation.create(day: Date.current - 1, admin_id: admin.id, user_id: user.id, start_time: Date.current + 2)
       reservation.valid?
-      expect(reservation.errors[:day]).to include("は過去の日付は選択できません")
+      expect(reservation.errors[:day]).to include("は過去の日付を選択できません")
     end
 
     it "dayが当日の場合は登録できないこと" do
